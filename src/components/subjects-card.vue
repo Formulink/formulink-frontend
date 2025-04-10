6ebe545cbaffe3a71d2e401809106283d6883625
laynexx <argo150808@gmail.com>
2025-04-10 23:33:02 +0300

    fix(subjects): fixed px and data fetching

--- src/components/subjects-card.vue
+++ src/components/subjects-card.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import {PlusIcon} from 'lucide-vue-next'
+import navigateTo from '@/funcs/navigate.ts'
 
+const props = defineProps({
+  id: Number,
+  name: String,
+})
 </script>
 
 <template>
-  <div>
+  <div @click="navigateTo(`/${id}/`)" class="rounded-3xl bg-white p-8 flex gap-8 items-center ">
+    <div class="flex justify-center items-center p-6 rounded-full bg-gradient-to-b from-violet-300 to-violet-500">
+      <PlusIcon/>
+    </div>
 
+    <h1 class="text-4xl">
+      {{props.name}}
+    </h1>
   </div>
 </template>
 
--- src/pages/Subjects.vue
+++ src/pages/Subjects.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-
 import Icon_standalone from '/public/icon_standalone.vue'
 import SubjectsCard from '@/components/subjects-card.vue'
+import { onMounted, ref } from 'vue'
+import type { Subject } from '@/types/subject.ts'
+
+const subjects = ref<Subject[]>([])
+
+onMounted(async ()=>{
+  const response = await fetch("http://localhost:8082/subjects")
+  subjects.value = await response.json()
+
+})
 </script>
 
 
@@ -10,8 +19,15 @@ import SubjectsCard from '@/components/subjects-card.vue'
   <div class="w-screen h-screen flex flex-col p-8 gap-6">
     <icon_standalone class="size-12"/>
 
-    <SubjectsCard
-
+    <div v-if="subjects" class=" w-full flex flex-col gap-6">
+      <h1 class="font-bold text-5xl">Предметы</h1>
+      <SubjectsCard
+        v-for="(item, index) in subjects"
+        :key="index"
+        :name = item.name
+        :id = item.id
+      />
+    </div>
   </div>
 
 </template>
